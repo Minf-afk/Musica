6b85fb8f374bc5bca3ca1f1d9f576582c01a725a
Mateus de Santana Pereira <msantana.inf@gmail.com>
2026-02-16 13:17:19 -0300

    Adicionar arquivos de projeto.

---
+++ Musica/Episodio.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace Musica
+{
+    public class Episodio
+    {
+        public int Duracao { get; set; }
+        public int Ordem { get; }
+        public string Descri { get; set; }
+        public string Titulo { get; set; }
+
+        public List<string> Convidado { get; } = new();
+
+        public Episodio(int dur, int ordem, string descri, string titulo)
+        {
+            Duracao = dur;
+            Ordem = ordem;
+            Descri = descri;
+            Titulo = titulo;
+        }
+
+        void AdicionarConvidados(string nome)
+        {
+            if(!string.IsNullOrWhiteSpace(nome))
+                Convidado.Add(nome);
+        }
+
+    }
+}
---
+++ Musica/Podcast.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Musica
+{
+    public class Podcast
+    {
+        public string Host { get; }
+        public string Nome { get; }
+        public int TotalEpisodios { get; private set; }
+
+        public Podcast(string host, string nome)
+        {
+            Host = host;
+            Nome = nome;
+            TotalEpisodios = 0;
+        }
+
+        public void AdicionarEpisodio()
+        {
+            TotalEpisodios++;
+        }
+
+        public void ExibirDetalhes()
+        {
+            AdicionarEpisodio();
+            Console.WriteLine($"Host: {Host} do dia, dando inicio ao episódio do dia: {Nome}\n");
+            Console.WriteLine($"Total de episódios: {TotalEpisodios}");
+        }
+
+    }
+}
